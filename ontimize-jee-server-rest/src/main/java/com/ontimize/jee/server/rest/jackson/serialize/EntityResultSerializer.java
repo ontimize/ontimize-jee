73f1b39bb75f66082f3a66efe5c8fd9cfa73e3a0
faustino.lage <faustino.lage@imatia.com>
2017-01-27 14:40:34 +0000

    Share project "ontimize-jee" into "https://svn.imatia.com/ontimize"
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee/trunk@3142 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

---
+++ ontimize-jee-server-rest/src/main/java/com/ontimize/jee/server/rest/jackson/serialize/EntityResultSerializer.java
@@ -0,0 +1,67 @@
+package com.ontimize.jee.server.rest.jackson.serialize;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.ontimize.db.EntityResult;
+
+public class EntityResultSerializer extends StdSerializer<EntityResult> {
+
+	public static final String CODE_KEY = "code";
+	public static final String MESSAGE_KEY = "message";
+	public static final String DATA_KEY = "data";
+	public static final String SQL_TYPES_KEY = "sqlTypes";
+
+	public EntityResultSerializer() {
+		super(EntityResult.class);
+	}
+
+	@Override
+	public void serialize(EntityResult value, JsonGenerator jgen, SerializerProvider provider)
+			throws IOException, JsonGenerationException {
+		jgen.writeStartObject();
+		jgen.writeNumberField(CODE_KEY, value.getCode());
+		jgen.writeStringField(MESSAGE_KEY, value.getMessage() != null ? value.getMessage() : "");
+		jgen.writeFieldName(DATA_KEY);
+		
+		int number = value.calculateRecordNumber();
+		
+		if( number != 0){
+			
+			jgen.writeStartArray();
+			
+			// EntityResult has values
+			for(int i=0;i<number;i++){
+				
+				Hashtable record = value.getRecordValues(i);
+				jgen.writeObject(record);
+			}
+			jgen.writeEndArray();
+		}
+		else{
+			// Check if EntityResult is the result of one insertion
+			Hashtable<String, Object> data = new Hashtable<String, Object>();
+			Enumeration<Object> keys = value.keys();
+			
+			while( keys.hasMoreElements() ){
+				
+				String itemKey = (String) keys.nextElement();
+				
+				data.put( itemKey, value.get(itemKey) );
+			}
+			
+			jgen.writeObject(data);
+		}
+		
+		jgen.writeFieldName(SQL_TYPES_KEY);
+		Hashtable sqlTypes = value.getColumnSQLTypes();
+		jgen.writeObject(sqlTypes);
+		jgen.writeEndObject();
+	}
+
+}
