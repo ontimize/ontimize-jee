f8f6b8d0bf0064c1b7bc12b8d96c1f2f92b07b7e
faustino.lage <faustino.lage@imatia.com>
2017-02-27 10:38:48 +0000

    Mejoras
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee/trunk@3172 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

---
+++ ontimize-jee-desktopclient/src/main/java/com/ontimize/jee/desktopclient/components/treetabbedformmanager/levelmanager/builder/LevelManagerBuilder.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+package com.ontimize.jee.desktopclient.components.treetabbedformmanager.levelmanager.builder;
+
+import java.util.ResourceBundle;
+
+import com.ontimize.gui.Form;
+import com.ontimize.jee.desktopclient.components.treetabbedformmanager.levelmanager.LevelManager;
+
+/**
+ * The Interface LevelManagerBuilder.
+ */
+public interface LevelManagerBuilder {
+
+    /**
+     * Builds the level manager.
+     * 
+     * @param uriFile
+     *            the uri file
+     * @param formManager
+     *            the form manager
+     * @param resourceBundle
+     *            the resource bundle
+     * @return the level manager
+     */
+    LevelManager buildLevelManager(String uriFile, Form formManager, ResourceBundle resourceBundle);
+
+    /**
+     * Builds the level manager.
+     * 
+     * @param content
+     *            the content
+     * @param formManager
+     *            the form manager
+     * @param resourceBundle
+     *            the resource bundle
+     * @return the level manager
+     */
+    LevelManager buildLevelManager(StringBuffer content, Form formManager, ResourceBundle resourceBundle);
+}
---
+++ ontimize-jee-desktopclient/src/main/java/com/ontimize/jee/desktopclient/locator/OJeeClientPermissionLocator.java
@@ -0,0 +1,146 @@
+package com.ontimize.jee.desktopclient.locator;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.net.URI;
+import java.rmi.Remote;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.AnnotatedGenericBeanDefinition;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+
+import com.ontimize.db.EntityResult;
+import com.ontimize.gui.ApplicationManager;
+import com.ontimize.gui.ClientWatch;
+import com.ontimize.gui.MessageDialog;
+import com.ontimize.jee.common.exceptions.InvalidCredentialsException;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.common.security.ILoginProvider;
+import com.ontimize.jee.common.services.user.IUserInformationService;
+import com.ontimize.jee.common.services.user.UserInformation;
+import com.ontimize.jee.common.tools.ReflectionTools;
+import com.ontimize.jee.desktopclient.i18n.I18NNaming;
+import com.ontimize.jee.desktopclient.locator.security.OntimizeLoginProvider;
+import com.ontimize.jee.desktopclient.spring.BeansFactory;
+import com.ontimize.locator.PermissionReferenceLocator;
+import com.ontimize.locator.PermissionSecureReferenceLocator;
+import com.ontimize.security.ClientSecurityManager;
+
+/**
+ * Clase abstracta que define el comportamiento que deben tener los ClientPermissionLocator contra un servidor JEE.
+ *
+ * @author joaquin.romero
+ *
+ */
+public class OJeeClientPermissionLocator extends PermissionReferenceLocator {
+
+	private static final Logger	logger								= LoggerFactory.getLogger(OJeeClientPermissionLocator.class);
+	private static final String	REMOTE_LOCATOR_INVOCATION_HANDLER	= "remoteLocatorInvocationHandler";
+
+	// protected String authorizationToken;
+	protected UserInformation	userInformation;
+
+	public OJeeClientPermissionLocator(Hashtable params) {
+		super(params);
+		InvocationHandler handler = BeansFactory.getBean(OJeeClientPermissionLocator.REMOTE_LOCATOR_INVOCATION_HANDLER, InvocationHandler.class);
+		if (handler == null) {
+			handler = ReflectionTools.newInstance((String) params.get(OJeeClientPermissionLocator.REMOTE_LOCATOR_INVOCATION_HANDLER),
+					InvocationHandler.class);
+		}
+		this.setRemoteLocatorInvocationHandler(handler);
+	}
+
+	/**
+	 * Session start
+	 */
+	@Override
+	public int startSession(String user, String password, ClientWatch cw) throws Exception {
+		ILoginProvider bean = null;
+		try {
+			try {
+				bean = BeansFactory.getBean(ILoginProvider.class);
+			} catch (NoSuchBeanDefinitionException ex) {
+				BeansFactory.registerBeanDefinition("loginProvider", new AnnotatedGenericBeanDefinition(OntimizeLoginProvider.class));
+				// yes, duplicate line. seems spring bug... review when possible
+				BeansFactory.registerBeanDefinition("loginProvider", new AnnotatedGenericBeanDefinition(OntimizeLoginProvider.class));
+				bean = BeansFactory.getBean(ILoginProvider.class);
+			}
+			String url = System.getProperty("com.ontimize.services.baseUrl");
+			URI uri = new URI(url);
+			bean.doLogin(uri, user, password);
+			IUserInformationService userService = BeansFactory.getBean(IUserInformationService.class);
+			this.userInformation = userService.getUserInformation();
+			if (this.userInformation == null) {
+				throw new AuthenticationCredentialsNotFoundException(user);
+			}
+			// Save password in local
+			this.userInformation.setPassword(password);
+		} catch (InvalidCredentialsException ex) {
+			throw new SecurityException("E_LOGIN__INVALID_CREDENTIALS", ex);
+		} catch (Exception ex) {
+			throw new SecurityException("E_LOGIN__ERROR", ex);
+		}
+		return super.startSession(user, password, cw);
+	}
+
+	/**
+	 * Establish the server locator througth a proxy
+	 *
+	 * @param remoteLocatorInvocationHandler
+	 *            the invocation handler
+	 */
+	public void setRemoteLocatorInvocationHandler(InvocationHandler remoteLocatorInvocationHandler) {
+		if (remoteLocatorInvocationHandler == null) {
+			throw new OntimizeJEERuntimeException(I18NNaming.E_REMOTE_LOCATOR_INVOCATIONHANDLER_MUST_BE_DEFINED);
+		}
+		this.referenceLocatorServer = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+				ReflectionTools.getInterfacesExtending(PermissionSecureReferenceLocator.class, Remote.class), remoteLocatorInvocationHandler);
+	}
+
+	@Override
+	protected void ensureRemoteReferenceLocator() throws Exception {
+		if (this.referenceLocatorServer == null) {
+			throw new OntimizeJEERuntimeException(I18NNaming.E_REMOTE_LOCATOR_REFERENCE_NO_ESTABLISHED);
+		}
+		super.ensureRemoteReferenceLocator();
+	}
+
+	@Override
+	public EntityResult getClientPermissions(Hashtable userKeys, int userId) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void installClientPermissions(Hashtable keys, int sessionId) throws Exception {
+		if (this.clientPermissionsInstalled) {
+			OJeeClientPermissionLocator.logger.warn("Client permissions are already installed. No new permission can be installed.");
+			return;
+		}
+
+		Map<String, ?> clientPermissions = this.userInformation.getClientPermissions();
+		if (clientPermissions != null) {
+			ApplicationManager.setClientSecurityManager(new ClientSecurityManager(Hashtable.class.cast(clientPermissions)));
+			this.clientPermissionsInstalled = true;
+			this.updateHourServerThread = ReflectionTools.newInstance(TimeThread.class, this);
+			this.updateHourServerThread.start();
+		} else {
+			MessageDialog.showMessage((JDialog) null,
+					"Error retrieving client permissions from column : " + this.clientPermissionsColumn + ", : Returned NULL value",
+					JOptionPane.ERROR_MESSAGE, null);
+			OJeeClientPermissionLocator.logger.error("Error retrieving client permissions from column : {} . Returned NULL value",
+					this.clientPermissionsColumn);
+		}
+	}
+
+	public UserInformation getUserInformation() {
+		return this.userInformation;
+	}
+
+}
