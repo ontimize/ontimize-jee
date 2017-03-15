a8d2d033e7d0bd0307a13172b80700b399b621f0
faustino.lage <faustino.lage@imatia.com>
2017-03-15 15:56:29 +0000

    Ultima actualizacion de ontimize-jee
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee/trunk@3217 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

--- ontimize-jee-common/src/main/java/com/ontimize/jee/common/cache/AbstractGenericCache.java
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/cache/AbstractGenericCache.java
@@ -12,120 +12,133 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * The Class GenericCache.
  *
- * @param <K> the key type
- * @param <V> the value type
+ * @param <K>
+ *            the key type
+ * @param <V>
+ *            the value type
  * @author <a href="user@email.com">Author</a>
  */
 public abstract class AbstractGenericCache<K, V> {
-    private static final long DEFAULT_TTL = 30 * 60 * 1000; // 30 minutes
-    private static final Logger logger = LoggerFactory.getLogger(AbstractGenericCache.class);
+	private static final long		DEFAULT_TTL	= 30l * 60l * 1000l;									// 30 minutes
+	private static final Logger		logger		= LoggerFactory.getLogger(AbstractGenericCache.class);
 
 	protected long					ttl;
 	protected Map<K, CachedItem<V>>	cache;
 
-    /**
-     * Instantiates a new generic cache.
-     */
-    public AbstractGenericCache() {
-        this(AbstractGenericCache.DEFAULT_TTL);
-    }
+	/**
+	 * Instantiates a new generic cache.
+	 */
+	public AbstractGenericCache() {
+		this(AbstractGenericCache.DEFAULT_TTL);
+	}
 
-    /**
-     * Instantiates a new generic cache.
-     *
-     * @param ttl the ttl
-     */
-    public AbstractGenericCache(final long ttl) {
-        this.ttl = ttl;
-        this.cache = new Hashtable<K, CachedItem<V>>();
-    }
+	/**
+	 * Instantiates a new generic cache.
+	 *
+	 * @param ttl
+	 *            the ttl
+	 */
+	public AbstractGenericCache(final long ttl) {
+		this.ttl = ttl;
+		this.cache = new Hashtable<K, CachedItem<V>>();
+	}
 
-    /**
-     * Obtiene.
-     *
-     * @param key the key
-     * @return the v
-     */
-    public V get(final K key) {
-        if ((this.ttl > 0) && this.cache.containsKey(key) &&
-                ((this.cache.get(key).getTimestamp() + this.ttl) < System.currentTimeMillis())) {
-            this.cache.remove(key);
-            AbstractGenericCache.logger.debug("Cache descartada para la clave: " + key);
-        }
-        if (!this.cache.containsKey(key)) {
-            final CachedItem<V> cachedItem = new CachedItem<V>();
-            final V result = this.requestData(key);
-            if (result != null) {
-                cachedItem.setValue(result);
-                cachedItem.setTimestamp(System.currentTimeMillis());
-                this.cache.put(key, cachedItem);
-                AbstractGenericCache.logger.debug("Cache cargada para clave: " + key + ". Valor: " + result);
-            } else {
-                return null;
-            }
-        } else {
-            AbstractGenericCache.logger.debug("Acierto de cache para la clave: " + key +
-                    ". Valor: " + this.cache.get(key).getValue());
-        }
-        return this.cache.get(key).getValue();
-    }
+	/**
+	 * Obtiene.
+	 *
+	 * @param key
+	 *            the key
+	 * @return the v
+	 */
+	public V get(final K key) {
 
-    /**
-     * Sets the ttl.
-     *
-     * @param ttl the new ttl
-     */
-    public void setTtl(final long ttl) {
-        this.ttl = ttl;
-    }
+		try {
+			CachedItem<V> cachedItem = this.cache.get(key);
+			if ((this.ttl > 0) && (cachedItem != null) && ((cachedItem.getTimestamp() + this.ttl) < System.currentTimeMillis())) {
+				this.cache.remove(key);
+				cachedItem = null;
+				AbstractGenericCache.logger.debug("Cache descartada para la clave: " + key);
+			}
+			if (cachedItem == null) {
+				cachedItem = new CachedItem<V>();
+				final V result = this.requestData(key);
+				if (result != null) {
+					cachedItem.setValue(result);
+					cachedItem.setTimestamp(System.currentTimeMillis());
+					this.cache.put(key, cachedItem);
+					AbstractGenericCache.logger.debug("Cache cargada para clave: " + key + ". Valor: " + result);
+				} else {
+					return null;
+				}
+			} else {
+				AbstractGenericCache.logger.debug("Acierto de cache para la clave: " + key + ". Valor: " + cachedItem.getValue());
+			}
+			return cachedItem.getValue();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 
-    /**
-     * Gets the ttl.
-     *
-     * @return the ttl
-     */
-    public long getTtl() {
-        return this.ttl;
-    }
+	/**
+	 * Sets the ttl.
+	 *
+	 * @param ttl
+	 *            the new ttl
+	 */
+	public void setTtl(final long ttl) {
+		this.ttl = ttl;
+	}
 
-    /**
-     * Invalidate cache.
-     */
-    public void invalidateCache() {
-        this.cache.clear();
-    }
+	/**
+	 * Gets the ttl.
+	 *
+	 * @return the ttl
+	 */
+	public long getTtl() {
+		return this.ttl;
+	}
 
-    /**
-     * Invalidate cache.
-     *
-     * @param key the key
-     */
-    public void invalidateCache(final K key) {
-        this.cache.remove(key);
-    }
+	/**
+	 * Invalidate cache.
+	 */
+	public void invalidateCache() {
+		this.cache.clear();
+	}
 
-    /**
-     * Put.
-     *
-     * @param key the key
-     * @param value the value
-     */
-    public void put(final K key, final V value) {
-        final CachedItem<V> item = new CachedItem<V>();
-        item.setTimestamp(System.currentTimeMillis());
-        item.setValue(value);
-        this.cache.put(key, item);
-    }
+	/**
+	 * Invalidate cache.
+	 *
+	 * @param key
+	 *            the key
+	 */
+	public void invalidateCache(final K key) {
+		this.cache.remove(key);
+	}
 
-    /**
-     * Request data.
-     *
-     * @param key the key
-     * @return the v
-     */
-    protected abstract V requestData(K key);
+	/**
+	 * Put.
+	 *
+	 * @param key
+	 *            the key
+	 * @param value
+	 *            the value
+	 */
+	public void put(final K key, final V value) {
+		final CachedItem<V> item = new CachedItem<V>();
+		item.setTimestamp(System.currentTimeMillis());
+		item.setValue(value);
+		this.cache.put(key, item);
+	}
+
+	/**
+	 * Request data.
+	 *
+	 * @param key
+	 *            the key
+	 * @return the v
+	 */
+	protected abstract V requestData(K key) throws Exception;
 }
--- ontimize-jee-common/src/main/java/com/ontimize/jee/common/cache/CachedItem.java
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/cache/CachedItem.java
@@ -9,47 +9,50 @@ package com.ontimize.jee.common.cache;
 /**
  * The Class CachedItem.
  *
- * @param <T> the generic type
+ * @param <T>
+ *            the generic type
  * @author <a href="user@email.com">Author</a>
  */
 public class CachedItem<T> {
-    private long timestamp;
-    private T value;
+	private long	timestamp;
+	private T		value;
 
-    /**
-     * Gets the timestamp.
-     *
-     * @return the timestamp
-     */
-    public long getTimestamp() {
-        return this.timestamp;
-    }
+	/**
+	 * Gets the timestamp.
+	 *
+	 * @return the timestamp
+	 */
+	public long getTimestamp() {
+		return this.timestamp;
+	}
 
-    /**
-     * Sets the timestamp.
-     *
-     * @param timestamp the new timestamp
-     */
-    public void setTimestamp(final long timestamp) {
-        this.timestamp = timestamp;
-    }
+	/**
+	 * Sets the timestamp.
+	 *
+	 * @param timestamp
+	 *            the new timestamp
+	 */
+	public void setTimestamp(final long timestamp) {
+		this.timestamp = timestamp;
+	}
 
-    /**
-     * Sets the value.
-     *
-     * @param value the new value
-     */
-    public void setValue(final T value) {
-        this.value = value;
-    }
+	/**
+	 * Sets the value.
+	 *
+	 * @param value
+	 *            the new value
+	 */
+	public void setValue(final T value) {
+		this.value = value;
+	}
 
-    /**
-     * Gets the value.
-     *
-     * @return the value
-     */
-    public T getValue() {
-        return this.value;
-    }
+	/**
+	 * Gets the value.
+	 *
+	 * @return the value
+	 */
+	public T getValue() {
+		return this.value;
+	}
 
 }
