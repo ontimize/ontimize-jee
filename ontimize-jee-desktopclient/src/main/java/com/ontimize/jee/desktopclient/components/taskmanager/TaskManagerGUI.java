5e00e79373f623336c9409d0c24e2ac307176389
joaquin.romero <joaquin.romero@imatia.com>
2017-06-23 13:24:28 +0000

    exract test to testclass
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee/trunk@3404 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

--- ontimize-jee-desktopclient/src/main/java/com/ontimize/jee/desktopclient/components/taskmanager/TaskManagerGUI.java
+++ ontimize-jee-desktopclient/src/main/java/com/ontimize/jee/desktopclient/components/taskmanager/TaskManagerGUI.java
@@ -386,13 +386,4 @@ public class TaskManagerGUI extends javax.swing.JFrame {
 		return this.taskTable;
 	}
 
-	/**
-	 * The main method.
-	 *
-	 * @param args
-	 *            the arguments
-	 */
-	public static void main(String[] args) {
-		new TaskManagerGUI().setVisible(true);
-	}
 }
