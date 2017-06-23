cba08cc87f877aedf3fbe0bb477ebc48e4e5a7f6
joaquin.romero <joaquin.romero@imatia.com>
2017-06-23 08:34:29 +0000

    spring factory for reference servlet context
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee/trunk@3402 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

---
+++ ontimize-jee-server/src/main/java/com/ontimize/jee/server/spring/ServletContextFactory.java
@@ -0,0 +1,51 @@
+package com.ontimize.jee.server.spring;
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.web.context.ServletContextAware;
+
+/**
+ * A factory for creating ServletContext objects.
+ */
+public class ServletContextFactory implements FactoryBean<ServletContext>, ServletContextAware {
+
+	/** The servlet context. */
+	private ServletContext servletContext;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.beans.factory.FactoryBean#getObject()
+	 */
+	@Override
+	public ServletContext getObject() throws Exception {
+		return this.servletContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.beans.factory.FactoryBean#getObjectType()
+	 */
+	@Override
+	public Class<?> getObjectType() {
+		return ServletContext.class;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.beans.factory.FactoryBean#isSingleton()
+	 */
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.web.context.ServletContextAware#setServletContext(javax.servlet.ServletContext)
+	 */
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+}
