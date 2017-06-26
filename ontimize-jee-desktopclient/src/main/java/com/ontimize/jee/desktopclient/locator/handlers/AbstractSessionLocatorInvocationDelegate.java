bf0766e79441cfa92b2a8062eb559cafaf8eda33
joaquin.romero <joaquin.romero@imatia.com>
2017-06-26 06:54:08 +0000

    catch stateless server
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee/trunk@3409 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

--- ontimize-jee-desktopclient/src/main/java/com/ontimize/jee/desktopclient/locator/handlers/AbstractSessionLocatorInvocationDelegate.java
+++ ontimize-jee-desktopclient/src/main/java/com/ontimize/jee/desktopclient/locator/handlers/AbstractSessionLocatorInvocationDelegate.java
@@ -3,6 +3,10 @@
  */
 package com.ontimize.jee.desktopclient.locator.handlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+
 import com.ontimize.db.AdvancedEntity;
 import com.ontimize.db.DirectSQLQueryEntity;
 import com.ontimize.db.Entity;
@@ -17,6 +21,7 @@ import com.ontimize.locator.SecureEntityReferenceLocator;
  */
 public abstract class AbstractSessionLocatorInvocationDelegate extends AbstractInvocationDelegate implements SecureEntityReferenceLocator {
 
+	private static final Logger logger = LoggerFactory.getLogger(AbstractSessionLocatorInvocationDelegate.class);
 
 	@Override
 	public int startSession(String user, String password, ClientWatch client) throws Exception {
@@ -30,7 +35,11 @@ public abstract class AbstractSessionLocatorInvocationDelegate extends AbstractI
 
 	@Override
 	public void endSession(int id) throws Exception {
-		BeansFactory.getBean(ISessionService.class).closeSession();
+		try {
+			BeansFactory.getBean(ISessionService.class).closeSession();
+		} catch (NoSuchBeanDefinitionException error) {
+			AbstractSessionLocatorInvocationDelegate.logger.info("No session service found", error);
+		}
 	}
 
 	@Override
