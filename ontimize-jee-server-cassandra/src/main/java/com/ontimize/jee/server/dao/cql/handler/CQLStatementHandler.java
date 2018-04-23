88462d7a36a3cc0f0df16b8286b480dec94716a3
faustino.lage <faustino.lage@imatia.com>
2018-04-23 07:58:41 +0000

    nuevo modulo cassandra
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee/trunk@3802 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

---
+++ ontimize-jee-server-cassandra/src/main/java/com/ontimize/jee/server/dao/cql/handler/CQLStatementHandler.java
@@ -0,0 +1,17 @@
+package com.ontimize.jee.server.dao.cql.handler;
+
+import java.util.List;
+import java.util.Map;
+
+import com.datastax.driver.core.ResultSet;
+import com.ontimize.db.EntityResult;
+import com.ontimize.db.SQLStatementBuilder.SQLOrder;
+import com.ontimize.jee.server.dao.cql.CQLStatement;
+
+public interface CQLStatementHandler {
+
+	public void resultSetToEntityResult(ResultSet resultSet, EntityResult entityResult, List<String> columnNames) throws Exception;
+
+	public CQLStatement createSelectQuery(String table, List<?> requestedColumns, Map<?, ?> conditions, List<String> wildcards, List<SQLOrder> columnSorting);
+
+}
