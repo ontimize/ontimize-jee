469ccd28d7a2b40d6c55032b669b57a6df1a4e8c
Xoán Loureiro Santamaría <xoan.loureiro@imatiasl.lan>
2021-10-28 10:00:33 +0200

    Add multitenant support

---
+++ ontimize-jee-server/src/main/java/com/ontimize/jee/server/multitenant/store/TenantSettings.java
@@ -0,0 +1,53 @@
+package com.ontimize.jee.server.multitenant.store;
+
+public class TenantSettings {
+	private String tenantId;
+
+	private String driverClass;
+
+	private String jdbcUrl;
+
+	private String username;
+
+	private String password;
+
+	public String getDriverClass() {
+		return driverClass;
+	}
+
+	public void setDriverClass(String driverClass) {
+		this.driverClass = driverClass;
+	}
+
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	public String getJdbcUrl() {
+		return jdbcUrl;
+	}
+
+	public void setJdbcUrl(String jdbcUrl) {
+		this.jdbcUrl = jdbcUrl;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
