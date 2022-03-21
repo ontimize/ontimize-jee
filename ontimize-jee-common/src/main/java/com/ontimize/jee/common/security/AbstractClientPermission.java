218718f883d6c88a5c465e653fcb27c9ebf5991f
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-03-21 13:50:53 +0100

    Fix Sonar Quality

--- ontimize-jee-common/src/main/java/com/ontimize/jee/common/security/AbstractClientPermission.java
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/security/AbstractClientPermission.java
@@ -1,112 +1,109 @@
 package com.ontimize.jee.common.security;
 
-import com.ontimize.jee.common.util.calendar.TimePeriod;
-
 import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ontimize.jee.common.util.calendar.TimePeriod;
 
 public class AbstractClientPermission implements RestrictedClientPermission {
 
-    protected boolean restricted = false;
+	private static final Logger logger = LoggerFactory.getLogger(AbstractClientPermission.class);
 
-    protected String name = null;
+	protected boolean restricted = false;
 
-    protected String attr = null;
+	protected String name = null;
 
-    protected TimePeriod period = null;
+	protected String attr = null;
 
-    @Override
-    public String getAttribute() {
-        return this.attr;
-    }
+	protected TimePeriod period = null;
 
-    @Override
-    public String getPermissionName() {
-        return this.name;
-    }
+	@Override
+	public String getAttribute() {
+		return this.attr;
+	}
 
-    public boolean isRestricted() {
-        return this.restricted;
-    }
+	@Override
+	public String getPermissionName() {
+		return this.name;
+	}
 
-    public void setRestricted(boolean restricted) {
-        this.restricted = restricted;
-    }
+	@Override
+	public boolean isRestricted() {
+		return this.restricted;
+	}
 
-    /**
-     * Returns true if the permission is a period restricted permission
-     * @return true if is a period restricted permission.
-     */
-    public boolean isPeriodRestricted() {
-        if (this.period != null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	@Override
+	public void setRestricted(boolean restricted) {
+		this.restricted = restricted;
+	}
 
-    @Override
-    public boolean hasPermission() {
-        // If time is in period and restricted and period are enabled, it won't
-        // permission
-        if (this.restricted) {
-            if (this.period != null) {
-                if (this.period.timeIsInPeriod(getTime())) {
-                    return false;
-                }
-                return true;
-            } else {
-                return !this.restricted;
-            }
-        } else {
-            if (this.period != null) {
-                if (this.period.timeIsInPeriod(getTime())) {
-                    return true;
-                }
-                return false;
-            } else {
-                return !this.restricted;
-            }
-        }
-    }
+	/**
+	 * Returns true if the permission is a period restricted permission
+	 * @return true if is a period restricted permission.
+	 */
+	@Override
+	public boolean isPeriodRestricted() {
+		return this.period != null;
+	}
 
-    @Override
-    public void setAttribute(String attr) {
-        this.attr = attr;
-    }
+	@Override
+	public boolean hasPermission() {
+		// If time is in period and restricted and period are enabled, it won't
+		// permission
+		if (this.restricted) {
+			if (this.period == null) {
+				return !this.restricted;
+			}
 
-    @Override
-    public TimePeriod getPeriod() {
-        return this.period;
-    }
+			return !this.period.timeIsInPeriod(this.getTime());
 
-    public void setPeriod(TimePeriod period) {
-        this.period = period;
-    }
+		}
+		if (this.period == null) {
+			return !this.restricted;
+		}
 
-    @Override
-    public String toString() {
-        String aux = this.getClass().toString() + " " + this.attr;
-        if (aux == null) {
-            aux = "";
-        }
-        StringBuilder sb = new StringBuilder(aux);
-        sb.append(" ");
-        sb.append(this.name);
-        sb.append(" " + this.period);
-        return sb.toString();
-    }
+		return this.period.timeIsInPeriod(this.getTime());
+
+	}
+
+	@Override
+	public void setAttribute(String attr) {
+		this.attr = attr;
+	}
+
+	@Override
+	public TimePeriod getPeriod() {
+		return this.period;
+	}
+
+	@Override
+	public void setPeriod(TimePeriod period) {
+		this.period = period;
+	}
+
+	@Override
+	public String toString() {
+		String aux = this.getClass().toString() + " " + this.attr;
+		StringBuilder sb = new StringBuilder(aux);
+		sb.append(" ");
+		sb.append(this.name);
+		sb.append(" " + this.period);
+		return sb.toString();
+	}
 
 
-    public long getTime() {
-        try {
-            Class clazz = Class.forName("com.ontimize.jee.common.gui.ApplicationManager");
-            Method method = clazz.getMethod("getTime", new Class[] {});
-            long time = (Long) method.invoke(null, new Object[] {});
-            return time;
-        } catch (Exception e) {
-        }
+	public long getTime() {
+		try {
+			Class<?> clazz = Class.forName("com.ontimize.jee.common.gui.ApplicationManager");
+			Method method = clazz.getMethod("getTime");
+			return (Long) method.invoke(null);
+		} catch (Exception e) {
+			AbstractClientPermission.logger.error(e.getMessage(), e);
+		}
 
-        return System.currentTimeMillis();
-    }
+		return System.currentTimeMillis();
+	}
 
 }
--- ontimize-jee-common/src/main/java/com/ontimize/jee/common/tools/ertools/AbstractAggregateFunction.java
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/tools/ertools/AbstractAggregateFunction.java
@@ -5,27 +5,27 @@ import java.util.List;
 
 public abstract class AbstractAggregateFunction<T extends IPartialAggregateValue> implements IAggregateFunction<T> {
 
-    private final String opColumn;
+	private final String opColumn;
 
-    private final String resultColumn;
+	private final String resultColumn;
 
-    public AbstractAggregateFunction(String opColumnName, String resultColumnName) {
-        super();
-        this.opColumn = opColumnName;
-        this.resultColumn = resultColumnName;
-    }
+	protected AbstractAggregateFunction(String opColumnName, String resultColumnName) {
+		super();
+		this.opColumn = opColumnName;
+		this.resultColumn = resultColumnName;
+	}
 
-    public String getOpColumn() {
-        return this.opColumn;
-    }
+	public String getOpColumn() {
+		return this.opColumn;
+	}
 
-    public String getResultColumn() {
-        return this.resultColumn;
-    }
+	public String getResultColumn() {
+		return this.resultColumn;
+	}
 
-    @Override
-    public List<String> getAggregatedColumnNames() {
-        return Arrays.asList(new String[] { this.resultColumn });
-    }
+	@Override
+	public List<String> getAggregatedColumnNames() {
+		return Arrays.asList(this.resultColumn);
+	}
 
 }
