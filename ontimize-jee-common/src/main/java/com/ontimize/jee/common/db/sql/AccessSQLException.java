b6ad8f34618c863c1cdd3142870a14d2353067e0
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-03-22 10:10:25 +0100

    Fix Sonar Quality

---
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/db/sql/AccessSQLException.java
@@ -0,0 +1,9 @@
+package com.ontimize.jee.common.db.sql;
+
+public class AccessSQLException extends Exception {
+
+	public AccessSQLException(String message) {
+		super(message);
+	}
+
+}
--- ontimize-jee-common/src/main/java/com/ontimize/jee/common/settings/AbstractSettingsHelper.java
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/settings/AbstractSettingsHelper.java
@@ -12,90 +12,90 @@ import com.ontimize.jee.common.tools.ParseUtilsExtended;
  */
 public abstract class AbstractSettingsHelper implements ISettingsHelper {
 
-    /** The Constant logger. */
-    private static final Logger logger = LoggerFactory.getLogger(AbstractSettingsHelper.class);
+	/** The Constant logger. */
+	private static final Logger logger = LoggerFactory.getLogger(AbstractSettingsHelper.class);
 
-    /** The Constant SETTING_KEY. */
-    protected final static String SETTING_KEY = "SET_KEY";
+	/** The Constant SETTING_KEY. */
+	protected static final String SETTING_KEY = "SET_KEY";
 
-    /** The Constant SETTING_VALUE. */
-    protected final static String SETTING_VALUE = "SET_VALUE";
+	/** The Constant SETTING_VALUE. */
+	protected static final String SETTING_VALUE = "SET_VALUE";
 
-    /**
-     * Instantiates a new settings helper.
-     */
-    public AbstractSettingsHelper() {
-        super();
-    }
+	/**
+	 * Instantiates a new settings helper.
+	 */
+	protected AbstractSettingsHelper() {
+		super();
+	}
 
-    /**
-     * Query.
-     * @param key the key
-     * @return the entity result
-     * @throws ApplicationException the application exception
-     * @throws OntimizeJEEException
-     */
-    protected abstract String query(String key) throws OntimizeJEEException;
+	/**
+	 * Query.
+	 * @param key the key
+	 * @return the entity result
+	 * @throws ApplicationException the application exception
+	 * @throws OntimizeJEEException
+	 */
+	protected abstract String query(String key) throws OntimizeJEEException;
 
-    /**
-     * Gets the string.
-     * @param key the key
-     * @param defaultValue the default value
-     * @return the string
-     */
-    @Override
-    public String getString(String key, String defaultValue) {
-        try {
-            String resValue = this.query(key);
-            return ObjectTools.coalesce(resValue, defaultValue);
-        } catch (Exception e) {
-            AbstractSettingsHelper.logger.warn("E_LOOKING_FOR_PROPERTY_WITH_KEY_{}", key, e);
-            return defaultValue;
-        }
-    }
+	/**
+	 * Gets the string.
+	 * @param key the key
+	 * @param defaultValue the default value
+	 * @return the string
+	 */
+	@Override
+	public String getString(String key, String defaultValue) {
+		try {
+			String resValue = this.query(key);
+			return ObjectTools.coalesce(resValue, defaultValue);
+		} catch (Exception e) {
+			AbstractSettingsHelper.logger.warn("E_LOOKING_FOR_PROPERTY_WITH_KEY_{}", key, e);
+			return defaultValue;
+		}
+	}
 
-    /**
-     * Gets the integer.
-     * @param key the key
-     * @param defaultValue the default value
-     * @return the integer
-     */
-    @Override
-    public Integer getInteger(String key, Integer defaultValue) {
-        return ParseUtilsExtended.getInteger(this.getString(key, null), defaultValue);
-    }
+	/**
+	 * Gets the integer.
+	 * @param key the key
+	 * @param defaultValue the default value
+	 * @return the integer
+	 */
+	@Override
+	public Integer getInteger(String key, Integer defaultValue) {
+		return ParseUtilsExtended.getInteger(this.getString(key, null), defaultValue);
+	}
 
-    /**
-     * Gets the boolean.
-     * @param key the key
-     * @param defaultValue the default value
-     * @return the boolean
-     */
-    @Override
-    public Boolean getBoolean(String key, Boolean defaultValue) {
-        return ParseUtilsExtended.getBoolean(this.getString(key, null), defaultValue);
-    }
+	/**
+	 * Gets the boolean.
+	 * @param key the key
+	 * @param defaultValue the default value
+	 * @return the boolean
+	 */
+	@Override
+	public Boolean getBoolean(String key, Boolean defaultValue) {
+		return ParseUtilsExtended.getBoolean(this.getString(key, null), defaultValue);
+	}
 
-    /**
-     * Gets the long.
-     * @param key the key
-     * @param defaultValue the default value
-     * @return the long
-     */
-    @Override
-    public Long getLong(String key, Long defaultValue) {
-        return ParseUtilsExtended.getLong(this.getString(key, null), defaultValue);
-    }
+	/**
+	 * Gets the long.
+	 * @param key the key
+	 * @param defaultValue the default value
+	 * @return the long
+	 */
+	@Override
+	public Long getLong(String key, Long defaultValue) {
+		return ParseUtilsExtended.getLong(this.getString(key, null), defaultValue);
+	}
 
-    /**
-     * Gets the double.
-     * @param key the key
-     * @param defaultValue the default value
-     * @return the double
-     */
-    @Override
-    public Double getDouble(String key, Long defaultValue) {
-        return ParseUtilsExtended.getDouble(this.getString(key, null), defaultValue);
-    }
+	/**
+	 * Gets the double.
+	 * @param key the key
+	 * @param defaultValue the default value
+	 * @return the double
+	 */
+	@Override
+	public Double getDouble(String key, Long defaultValue) {
+		return ParseUtilsExtended.getDouble(this.getString(key, null), defaultValue);
+	}
 
 }
