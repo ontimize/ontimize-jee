a59f76427f36ec77ac5fa5560742e4f77471fd7c
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-03-22 10:57:24 +0100

    Fix Sonar Quality

---
+++ ontimize-jee-common/src/main/resources/com/ontimize/jee/common/settings/AbstractSettingsHelperTest.java
@@ -0,0 +1,42 @@
+package com.ontimize.jee.common.settings;
+
+import org.junit.jupiter.api.Test;
+
+public class AbstractSettingsHelperTest {
+
+	@Test
+	public void testAbstractSettingsHelper() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testQuery() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetString() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetInteger() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetBoolean() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetLong() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetDouble() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
