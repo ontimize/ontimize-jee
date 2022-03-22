7a1f829e5a7595f8f8580c20fb8012f96c3d2c70
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-03-22 15:40:25 +0100

    Fix Sonar Quality

--- ontimize-jee-common/src/main/java/com/ontimize/jee/common/util/calendar/AdvancedTimePeriod.java
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/util/calendar/AdvancedTimePeriod.java
@@ -1,265 +1,263 @@
 package com.ontimize.jee.common.util.calendar;
 
-import com.ontimize.jee.common.util.ParseTools;
-
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 import java.util.List;
+import java.util.Locale;
 
+import com.ontimize.jee.common.util.ParseTools;
 
 public class AdvancedTimePeriod implements TimePeriod {
 
-    protected Calendar calendar = null;
+	protected Calendar calendar = null;
 
-    protected BusinessCalendar businessCalendar = null;
+	protected BusinessCalendar businessCalendar = null;
 
-    protected Locale locale;
+	protected Locale locale;
 
-    protected boolean isWorkingPeriod;
+	protected boolean isWorkingPeriod;
 
-    protected int startDay;
+	protected int startDay;
 
-    protected int endDay;
+	protected int endDay;
 
-    protected int startMonth;
+	protected int startMonth;
 
-    protected int endMonth;
+	protected int endMonth;
 
-    public AdvancedTimePeriod(String periodDefinitionString, Locale locale, String businessCalendarProperties)
-            throws Exception {
-        this.isWorkingPeriod = false;
-        this.locale = locale;
+	public AdvancedTimePeriod(String periodDefinitionString, Locale locale, String businessCalendarProperties) {
+		this.isWorkingPeriod = false;
+		this.locale = locale;
 
-        this.businessCalendar = new BusinessCalendar(businessCalendarProperties, locale);
-        this.calendar = Calendar.getInstance(locale);
+		this.businessCalendar = new BusinessCalendar(businessCalendarProperties, locale);
+		this.calendar = Calendar.getInstance(locale);
 
-        this.parseString(periodDefinitionString);
-    }
+		this.parseString(periodDefinitionString);
+	}
 
-    public AdvancedTimePeriod(int startDay, int endDay, int startMonth, int endMonth, boolean workingPeriod,
-            Locale locale, BusinessCalendar businessCalendar) {
-        this.startDay = startDay;
-        this.endDay = endDay;
-        this.startMonth = startMonth;
-        this.endMonth = endMonth;
-        this.isWorkingPeriod = workingPeriod;
+	public AdvancedTimePeriod(int startDay, int endDay, int startMonth, int endMonth, boolean workingPeriod,
+			Locale locale, BusinessCalendar businessCalendar) {
+		this.startDay = startDay;
+		this.endDay = endDay;
+		this.startMonth = startMonth;
+		this.endMonth = endMonth;
+		this.isWorkingPeriod = workingPeriod;
 
-        this.locale = locale;
-        this.businessCalendar = businessCalendar;
-        this.calendar = Calendar.getInstance(locale);
-    }
+		this.locale = locale;
+		this.businessCalendar = businessCalendar;
+		this.calendar = Calendar.getInstance(locale);
+	}
 
-    protected void parseString(String periodDefinitionString) {
-        List daysMonthsYears = ParseTools.getTokensAt(periodDefinitionString,
-                BasicTimePeriodParser.DAY_MONTH_SEPARATOR);
-        // This List has to contain at least two values with the days and
-        // months. Years are optional
-        if (daysMonthsYears.size() < 2) {
-            throw new IllegalArgumentException("Incorrect format: character / not found");
-        } else if (daysMonthsYears.size() > 3) {
-            throw new IllegalArgumentException(
-                    "Incorrect format: character / only can be used to separate days months and years");
-        }
+	protected void parseString(String periodDefinitionString) {
+		List<String> daysMonthsYears = ParseTools.getTokensAt(periodDefinitionString,
+				BasicTimePeriodParser.DAY_MONTH_SEPARATOR);
+		// This List has to contain at least two values with the days and
+		// months. Years are optional
+		if (daysMonthsYears.size() < 2) {
+			throw new IllegalArgumentException("Incorrect format: character / not found");
+		}
+		if (daysMonthsYears.size() > 3) {
+			throw new IllegalArgumentException(
+					"Incorrect format: character / only can be used to separate days months and years");
+		}
 
-        // Parse the day
-        String dayDefinition = (String) daysMonthsYears.get(0);
-        if (dayDefinition.startsWith(BasicTimePeriodParser.ALL)) {
-            this.startDay = -1;
-            this.endDay = -1;
-            if (dayDefinition.endsWith(BasicTimePeriodParser.WORKING_DAY)) {
-                this.isWorkingPeriod = true;
-            }
-        } else if (dayDefinition.indexOf(BasicTimePeriodParser.INTERVAL) > 0) {
-            List intervalDays = ParseTools.getTokensAt(dayDefinition, BasicTimePeriodParser.INTERVAL);
-            if (intervalDays.size() != 2) {
-                throw new IllegalArgumentException(
-                        "Incorrect format: character - must separate two numerical values to define the days");
-            }
+		// Parse the day
+		this.parseDays(daysMonthsYears);
 
-            String day1 = (String) intervalDays.get(0);
-            if (day1.endsWith(BasicTimePeriodParser.WORKING_DAY)) {
-                this.isWorkingPeriod = true;
-                day1 = day1.substring(0, day1.length() - 1);
-            }
-            this.startDay = Integer.parseInt(day1);
+		// Parse the month
+		this.parseMonths(daysMonthsYears);
+	}
 
-            String day2 = (String) intervalDays.get(1);
-            if (day2.endsWith(BasicTimePeriodParser.WORKING_DAY)) {
-                this.isWorkingPeriod = true;
-                day2 = day2.substring(0, day2.length() - 1);
-            }
-            this.endDay = Integer.parseInt(day2);
-        } else {
-            if (dayDefinition.endsWith(BasicTimePeriodParser.WORKING_DAY)) {
-                this.isWorkingPeriod = true;
-                dayDefinition = dayDefinition.substring(0, dayDefinition.length() - 1);
-            }
-            this.startDay = Integer.parseInt(dayDefinition);
-            this.endDay = this.startDay;
-        }
+	private void parseMonths(List<String> daysMonthsYears) {
+		String monthDefinition = daysMonthsYears.get(1);
+		if (monthDefinition.equals(BasicTimePeriodParser.ALL)) {
+			this.startMonth = Calendar.JANUARY;
+			this.endMonth = Calendar.DECEMBER;
+		} else if (monthDefinition.contains(BasicTimePeriodParser.INTERVAL)) {
+			List<String> intervalMonths = ParseTools.getTokensAt(monthDefinition, BasicTimePeriodParser.INTERVAL);
+			if (intervalMonths.size() != 2) {
+				throw new IllegalArgumentException(
+						"Incorrect format: character - must separate two numerical values to define the months");
+			}
+			this.startMonth = Integer.parseInt(intervalMonths.get(0)) - 1;
+			this.endMonth = Integer.parseInt(intervalMonths.get(1)) - 1;
+		} else {
+			this.startMonth = Integer.parseInt(monthDefinition) - 1;
+			this.endMonth = this.startMonth;
+		}
+	}
 
-        // Parse the month
-        String monthDefinition = (String) daysMonthsYears.get(1);
-        if (monthDefinition.equals(BasicTimePeriodParser.ALL)) {
-            this.startMonth = Calendar.JANUARY;
-            this.endMonth = Calendar.DECEMBER;
-        } else if (monthDefinition.indexOf(BasicTimePeriodParser.INTERVAL) > 0) {
-            List intervalMonths = ParseTools.getTokensAt(monthDefinition, BasicTimePeriodParser.INTERVAL);
-            if (intervalMonths.size() != 2) {
-                throw new IllegalArgumentException(
-                        "Incorrect format: character - must separate two numerical values to define the months");
-            }
-            this.startMonth = Integer.parseInt((String) intervalMonths.get(0)) - 1;
-            this.endMonth = Integer.parseInt((String) intervalMonths.get(1)) - 1;
-        } else {
-            this.startMonth = Integer.parseInt(monthDefinition) - 1;
-            this.endMonth = this.startMonth;
-        }
-    }
+	private void parseDays(List<String> daysMonthsYears) {
+		String dayDefinition = daysMonthsYears.get(0);
+		if (dayDefinition.startsWith(BasicTimePeriodParser.ALL)) {
+			this.startDay = -1;
+			this.endDay = -1;
+			if (dayDefinition.endsWith(BasicTimePeriodParser.WORKING_DAY)) {
+				this.isWorkingPeriod = true;
+			}
+		} else if (dayDefinition.contains(BasicTimePeriodParser.INTERVAL)) {
+			List<String> intervalDays = ParseTools.getTokensAt(dayDefinition, BasicTimePeriodParser.INTERVAL);
+			if (intervalDays.size() != 2) {
+				throw new IllegalArgumentException(
+						"Incorrect format: character - must separate two numerical values to define the days");
+			}
 
-    @Override
-    public boolean timeIsInPeriod(long time) {
-        // Set the date to check in the calendar object
-        Date d = new Date(time);
-        this.calendar.setTime(d);
+			String day1 = intervalDays.get(0);
+			if (day1.endsWith(BasicTimePeriodParser.WORKING_DAY)) {
+				this.isWorkingPeriod = true;
+				day1 = day1.substring(0, day1.length() - 1);
+			}
+			this.startDay = Integer.parseInt(day1);
 
-        boolean validate = this.validateMonth(this.calendar);
-        if (!validate) {
-            return false;
-        }
+			String day2 = intervalDays.get(1);
+			if (day2.endsWith(BasicTimePeriodParser.WORKING_DAY)) {
+				this.isWorkingPeriod = true;
+				day2 = day2.substring(0, day2.length() - 1);
+			}
+			this.endDay = Integer.parseInt(day2);
+		} else {
+			if (dayDefinition.endsWith(BasicTimePeriodParser.WORKING_DAY)) {
+				this.isWorkingPeriod = true;
+				dayDefinition = dayDefinition.substring(0, dayDefinition.length() - 1);
+			}
+			this.startDay = Integer.parseInt(dayDefinition);
+			this.endDay = this.startDay;
+		}
+	}
 
-        if (this.isWorkingPeriod) {
-            return this.timeIsInWorkingPeriod(this.calendar);
-        } else {
-            return this.timeIsInNormalPeriod(this.calendar);
-        }
-    }
+	@Override
+	public boolean timeIsInPeriod(long time) {
+		// Set the date to check in the calendar object
+		Date d = new Date(time);
+		this.calendar.setTime(d);
 
-    protected boolean timeIsInNormalPeriod(Calendar cal) {
-        if (this.startDay == -1) {
-            // All days are in the period
-            return true;
-        } else if (this.startDay == this.endDay) {
-            // There are only one day in the period
-            if (this.calendar.get(Calendar.DAY_OF_MONTH) == this.startDay) {
-                return true;
-            }
-            return false;
-        } else {
-            int calendarDay = this.calendar.get(Calendar.DAY_OF_MONTH);
-            return (calendarDay >= this.startDay) && (calendarDay <= this.endDay);
-        }
-    }
+		boolean validate = this.validateMonth(this.calendar);
+		if (!validate) {
+			return false;
+		}
 
-    protected boolean timeIsInWorkingPeriod(Calendar cal) {
+		if (this.isWorkingPeriod) {
+			return this.timeIsInWorkingPeriod(this.calendar);
+		}
+		return this.timeIsInNormalPeriod();
+	}
 
-        if (this.startDay == -1) {
-            // All working days are in the period
-            return true;
-        } else if (this.startDay == this.endDay) {
-            // There are only one day in the period
-            int iDay = 0;
-            int workingDays = 0;
-            while (workingDays < this.startDay) {
-                iDay++;
-                boolean holidays = this.businessCalendar.isHoliday(iDay, cal.get(Calendar.MONTH),
-                        cal.get(Calendar.YEAR));
-                if (!holidays) {
-                    workingDays++;
-                }
-            }
-            if (iDay == cal.get(Calendar.DAY_OF_MONTH)) {
-                return true;
-            }
-            return false;
-        } else {
-            int iDay = this.businessCalendar.workingDayOfMonthToDayOfMonth(this.startDay, cal.get(Calendar.MONTH),
-                    cal.get(Calendar.YEAR));
-            int iLastDay = this.businessCalendar.workingDayOfMonthToDayOfMonth(this.endDay, cal.get(Calendar.MONTH),
-                    cal.get(Calendar.YEAR));
-            if ((cal.get(Calendar.DAY_OF_MONTH) >= iDay) && (cal.get(Calendar.DAY_OF_MONTH) <= iLastDay)) {
-                return true;
-            }
-            return false;
-        }
-    }
+	protected boolean timeIsInNormalPeriod() {
+		if (this.startDay == -1) {
+			// All days are in the period
+			return true;
+		}
+		if (this.startDay == this.endDay) {
+			// There are only one day in the period
+			return this.calendar.get(Calendar.DAY_OF_MONTH) == this.startDay;
+		}
+		int calendarDay = this.calendar.get(Calendar.DAY_OF_MONTH);
+		return (calendarDay >= this.startDay) && (calendarDay <= this.endDay);
+	}
 
-    protected boolean validateMonth(Calendar cal) {
-        if (this.startMonth == -1) {
-            return true;
-        } else if (this.startMonth == this.endMonth) {
-            return cal.get(Calendar.MONTH) == this.startMonth;
-        } else {
-            int calendarMonth = cal.get(Calendar.MONTH);
-            return (calendarMonth >= this.startMonth) && (calendarMonth <= this.endMonth);
-        }
-    }
+	protected boolean timeIsInWorkingPeriod(Calendar cal) {
 
-    public boolean isWorkingPeriod() {
-        return this.isWorkingPeriod;
-    }
+		if (this.startDay == -1) {
+			// All working days are in the period
+			return true;
+		}
+		if (this.startDay != this.endDay) {
+			int iDay = this.businessCalendar.workingDayOfMonthToDayOfMonth(this.startDay, cal.get(Calendar.MONTH),
+					cal.get(Calendar.YEAR));
+			int iLastDay = this.businessCalendar.workingDayOfMonthToDayOfMonth(this.endDay, cal.get(Calendar.MONTH),
+					cal.get(Calendar.YEAR));
+			return ((cal.get(Calendar.DAY_OF_MONTH) >= iDay) && (cal.get(Calendar.DAY_OF_MONTH) <= iLastDay));
 
-    public BusinessCalendar getBusinessCalendar() {
-        return this.businessCalendar;
-    }
+		}
+		// There are only one day in the period
+		int iDay = 0;
+		int workingDays = 0;
+		while (workingDays < this.startDay) {
+			iDay++;
+			boolean holidays = this.businessCalendar.isHoliday(iDay, cal.get(Calendar.MONTH),
+					cal.get(Calendar.YEAR));
+			if (!holidays) {
+				workingDays++;
+			}
+		}
+		return iDay == cal.get(Calendar.DAY_OF_MONTH);
+	}
 
-    public Locale getLocale() {
-        return this.locale;
-    }
+	protected boolean validateMonth(Calendar cal) {
+		if (this.startMonth == -1) {
+			return true;
+		}
+		if (this.startMonth == this.endMonth) {
+			return cal.get(Calendar.MONTH) == this.startMonth;
+		}
+		int calendarMonth = cal.get(Calendar.MONTH);
+		return (calendarMonth >= this.startMonth) && (calendarMonth <= this.endMonth);
+	}
 
-    public int getStartDay() {
-        return this.startDay;
-    }
+	public boolean isWorkingPeriod() {
+		return this.isWorkingPeriod;
+	}
 
-    public int getEndDay() {
-        return this.endDay;
-    }
+	public BusinessCalendar getBusinessCalendar() {
+		return this.businessCalendar;
+	}
 
-    public int getStartMonth() {
-        return this.startMonth;
-    }
+	public Locale getLocale() {
+		return this.locale;
+	}
 
-    public int getEndMonth() {
-        return this.endMonth;
-    }
+	public int getStartDay() {
+		return this.startDay;
+	}
 
-    public String getPeriodString() {
-        StringBuilder result = new StringBuilder();
+	public int getEndDay() {
+		return this.endDay;
+	}
 
-        if (this.startDay == -1) {
-            result.append(BasicTimePeriodParser.ALL);
-        } else if (this.startDay == this.endDay) {
-            result.append(this.startDay);
-            if (this.isWorkingPeriod) {
-                result.append(BasicTimePeriodParser.WORKING_DAY);
-            }
-        } else {
-            result.append(this.startDay);
-            if (this.isWorkingPeriod) {
-                result.append(BasicTimePeriodParser.WORKING_DAY);
-            }
-            result.append(BasicTimePeriodParser.INTERVAL);
-            result.append(this.endDay);
-            if (this.isWorkingPeriod) {
-                result.append(BasicTimePeriodParser.WORKING_DAY);
-            }
-        }
+	public int getStartMonth() {
+		return this.startMonth;
+	}
 
-        result.append(BasicTimePeriodParser.DAY_MONTH_SEPARATOR);
+	public int getEndMonth() {
+		return this.endMonth;
+	}
 
-        if (this.startMonth == -1) {
-            result.append(BasicTimePeriodParser.ALL);
-        } else if (this.startMonth == this.endMonth) {
-            result.append(this.startMonth + 1);
-        } else {
-            result.append(this.startMonth + 1);
-            result.append(BasicTimePeriodParser.INTERVAL);
-            result.append(this.endMonth + 1);
-        }
+	public String getPeriodString() {
+		StringBuilder result = new StringBuilder();
 
-        return result.toString();
+		if (this.startDay == -1) {
+			result.append(BasicTimePeriodParser.ALL);
+		} else if (this.startDay == this.endDay) {
+			result.append(this.startDay);
+			if (this.isWorkingPeriod) {
+				result.append(BasicTimePeriodParser.WORKING_DAY);
+			}
+		} else {
+			result.append(this.startDay);
+			if (this.isWorkingPeriod) {
+				result.append(BasicTimePeriodParser.WORKING_DAY);
+			}
+			result.append(BasicTimePeriodParser.INTERVAL);
+			result.append(this.endDay);
+			if (this.isWorkingPeriod) {
+				result.append(BasicTimePeriodParser.WORKING_DAY);
+			}
+		}
 
-    }
+		result.append(BasicTimePeriodParser.DAY_MONTH_SEPARATOR);
+
+		if (this.startMonth == -1) {
+			result.append(BasicTimePeriodParser.ALL);
+		} else if (this.startMonth == this.endMonth) {
+			result.append(this.startMonth + 1);
+		} else {
+			result.append(this.startMonth + 1);
+			result.append(BasicTimePeriodParser.INTERVAL);
+			result.append(this.endMonth + 1);
+		}
+
+		return result.toString();
+
+	}
 
 }
--- ontimize-jee-common/src/main/java/com/ontimize/jee/common/util/extend/AliasPropertiesParser.java
+++ ontimize-jee-common/src/main/java/com/ontimize/jee/common/util/extend/AliasPropertiesParser.java
@@ -1,61 +1,71 @@
 package com.ontimize.jee.common.util.extend;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ontimize.jee.common.exceptions.OntimizeJEEException;
+
 public class AliasPropertiesParser extends PropertiesParser {
 
-    private static final Logger logger = LoggerFactory.getLogger(AliasPropertiesParser.class);
+	private static final Logger logger = LoggerFactory.getLogger(AliasPropertiesParser.class);
 
-    public static final String ADD_ALIAS_OP = "ADD_ALIAS";
+	public static final String ADD_ALIAS_OP = "ADD_ALIAS";
 
-    public static final String REMOVE_ALIAS_OP = "REMOVE_ALIAS";
+	public static final String REMOVE_ALIAS_OP = "REMOVE_ALIAS";
 
-    @Override
-    protected void executeOperation(Properties propertiesFile, String operation, String parameters,
-            String operationValues) throws Exception {
+	@Override
+	protected void executeOperation(Properties propertiesFile, String operation, String parameters,
+			String operationValues) throws Exception {
 
-        // ADD OPERATION
-        if (operation.equalsIgnoreCase(AliasPropertiesParser.ADD_ALIAS_OP)) {
-            ArrayList parametersList = this.getValuesList(parameters, VALUES_SEPARATOR);
-            ArrayList operationValuesList = this.getValuesList(operationValues, VALUES_SEPARATOR);
+		// ADD OPERATION
+		if (operation.equalsIgnoreCase(AliasPropertiesParser.ADD_ALIAS_OP)) {
+			this.executeAddOperation(propertiesFile, operation, parameters, operationValues);
+		}
 
-            if (parametersList.size() != operationValuesList.size()) {
-                throw new Exception(operation + ": PARAMETER_LENGHT_DISTINCT_VALUES_LENGHT");
-            }
+		// REMOVE OPERATION
+		if (operation.equalsIgnoreCase(AliasPropertiesParser.REMOVE_ALIAS_OP)) {
+			this.executeRemoveOperation(propertiesFile, parameters);
+		}
 
-            for (int i = 0; i < parametersList.size(); i++) {
-                String parameter = null;
-                String value = null;
-                try {
-                    parameter = (String) parametersList.get(i);
-                    value = (String) operationValuesList.get(i);
-                } catch (Exception e) {
-                    AliasPropertiesParser.logger.error(null, e);
-                }
+	}
 
-                if ((parameter != null) && (value != null)) {
-                    propertiesFile.put(parameter, value);
-                }
-            }
-        }
+	private void executeRemoveOperation(Properties propertiesFile, String parameters) {
+		ArrayList<String> parametersList = this.getValuesList(parameters, PropertiesParser.VALUES_SEPARATOR);
+		for (String parameter : parametersList) {
 
-        // REMOVE OPERATION
-        if (operation.equalsIgnoreCase(AliasPropertiesParser.REMOVE_ALIAS_OP)) {
+			if (parameter != null) {
+				propertiesFile.remove(parameter);
+			}
+		}
+	}
 
-            ArrayList parametersList = this.getValuesList(parameters, VALUES_SEPARATOR);
-            for (int i = 0; i < parametersList.size(); i++) {
-                String parameter = (String) parametersList.get(i);
+	private void executeAddOperation(Properties propertiesFile, String operation, String parameters,
+			String operationValues) throws OntimizeJEEException {
+		ArrayList<String> parametersList = this.getValuesList(parameters, PropertiesParser.VALUES_SEPARATOR);
+		ArrayList<String> operationValuesList = this.getValuesList(operationValues,
+				PropertiesParser.VALUES_SEPARATOR);
 
-                if (parameter != null) {
-                    propertiesFile.remove(parameter);
-                }
-            }
-        }
+		if (parametersList.size() != operationValuesList.size()) {
+			throw new OntimizeJEEException(operation + ": PARAMETER_LENGHT_DISTINCT_VALUES_LENGHT");
+		}
 
-    }
+		for (int i = 0; i < parametersList.size(); i++) {
+			String parameter = null;
+			String value = null;
+			try {
+				parameter = parametersList.get(i);
+				value = operationValuesList.get(i);
+			} catch (Exception e) {
+				AliasPropertiesParser.logger.error(null, e);
+			}
+
+			if ((parameter != null) && (value != null)) {
+				propertiesFile.put(parameter, value);
+			}
+		}
+	}
 
 }
