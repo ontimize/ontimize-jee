a6fd588f2154222fafe520e618d39b9b3df1bb5f
Lucía Fernández Nuñez <lucia.fernandez@imatia.com>
2022-11-07 10:09:06 +0100

    Modified class SQLServerSQLStatementHandlerTest adding ParameterizedTest

--- ontimize-jee-common/src/test/java/com/ontimize/jee/common/test/dbTest/SQLServerSQLStatementHandlerTest.java
+++ ontimize-jee-common/src/test/java/com/ontimize/jee/common/test/dbTest/SQLServerSQLStatementHandlerTest.java
@@ -3,7 +3,11 @@ package com.ontimize.jee.common.test.dbTest;
 import com.ontimize.jee.common.db.handler.SQLServerSQLStatementHandler;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -11,6 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -23,57 +28,53 @@ class SQLServerSQLStatementHandlerTest {
 
 
     @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     class CreateSelectQuery {
 
-
         @Test
-        void when_receive_table_and_requestedColumns_and_conditions_and_wildcards_and_columnsSorting_and_recordCount_and_descending_is_false_and_forceDistinct_is_false_expect_select_query() {
+        void when_receive_one_table_expect_select_query() {
+            var table = "my-table";
+            HashMap conditions = null;
+            ArrayList wildcards = null;
+            ArrayList requestedColumns = null;
 
+            var result = sqlServerSQLStatementHandler.createSelectQuery(table, null, null, null);
+            var expected = "SELECT  TOP 100 PERCENT  *  FROM  [my-table]";
+
+            assertEquals(expected, result.getSQLStatement().trim());
+        }
+
+
+        @ParameterizedTest
+        @MethodSource("addDataCreateSelectQuery")
+        void when_receive_table_and_requestedColumns_and_conditions_and_wildcards_and_columnsSorting_and_recordCount_and_descending_and_forceDistinct_expect_select_query(boolean descending, boolean forceDistinct, String expected) {
             var table = "my-table";
             ArrayList requestedColumns = new ArrayList();
             HashMap conditions = new HashMap();
             ArrayList wildcards = new ArrayList();
             ArrayList columnSorting = new ArrayList();
             int recordCount = 1;
-            boolean descending = false;
-            boolean forceDistinct = false;
+            int offset = 1;
+
 
             requestedColumns.add("requestedColumns1");
             conditions.put("field1", "value1");
             wildcards.add("wildcards1");
             columnSorting.add("columnSorting1");
 
-            var result = sqlServerSQLStatementHandler.createSelectQuery(table, requestedColumns, conditions, wildcards, columnSorting, recordCount, descending, forceDistinct);
-            var expected = "SELECT  TOP  1 requestedColumns1 , columnSorting1 FROM  [my-table]   WHERE field1 = ?  ORDER BY columnSorting1";
+            var result = sqlServerSQLStatementHandler.createSelectQuery(table, requestedColumns, conditions, wildcards, columnSorting, recordCount, offset, descending, forceDistinct);
 
             assertEquals(expected, result.getSQLStatement().trim());
         }
 
-        @Test
-        void when_receive_table_and_requestedColumns_and_conditions_and_wildcards_and_columnsSorting_and_recordCount_and_descending_is_true_and_forceDistinct_is_true_expect_select_query() {
-            var table = "my-table";
-            ArrayList requestedColumns = new ArrayList();
-            HashMap conditions = new HashMap();
-            ArrayList wildcards = new ArrayList();
-            ArrayList columnSorting = new ArrayList();
-            int recordCount = 1;
-            boolean descending = true;
-            boolean forceDistinct = true;
-
-            requestedColumns.add("requestedColumns1");
-            conditions.put("field1", "value1");
-            wildcards.add("wildcards1");
-            columnSorting.add("columnSorting1");
-
-            var result = sqlServerSQLStatementHandler.createSelectQuery(table, requestedColumns, conditions, wildcards, columnSorting, recordCount, descending, forceDistinct);
-            var expected = "SELECT  DISTINCT  TOP  1 requestedColumns1 , columnSorting1 FROM  [my-table]   WHERE field1 = ?  ORDER BY columnSorting1 DESC";
-
-            assertEquals(expected, result.getSQLStatement().trim());
+        Stream<Arguments> addDataCreateSelectQuery() {
+            return Stream.of(
+                    Arguments.of(true, false, "SELECT  TOP  2 requestedColumns1 , columnSorting1 FROM  [my-table]   WHERE field1 = ?  ORDER BY columnSorting1 DESC"),
+                    Arguments.of(true, true, "SELECT  DISTINCT  TOP  2 requestedColumns1 , columnSorting1 FROM  [my-table]   WHERE field1 = ?  ORDER BY columnSorting1 DESC"),
+                    Arguments.of(false, false, "SELECT  TOP  2 requestedColumns1 , columnSorting1 FROM  [my-table]   WHERE field1 = ?  ORDER BY columnSorting1"),
+                    Arguments.of(false, true, "SELECT  DISTINCT  TOP  2 requestedColumns1 , columnSorting1 FROM  [my-table]   WHERE field1 = ?  ORDER BY columnSorting1")
+            );
         }
-
-
-
-
 
     }
 
