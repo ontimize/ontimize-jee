52758f391ec10fd420153c4aaeff1533afa9d25c
Xoán Loureiro Santamaría <xoan.loureiro@imatia.com>
2023-05-16 08:20:38 +0200

    Allow to configure roles without a database

--- ontimize-jee-server-keycloak/src/main/java/com/ontimize/jee/server/security/keycloak/IOntimizeKeycloakConfiguration.java
+++ ontimize-jee-server-keycloak/src/main/java/com/ontimize/jee/server/security/keycloak/IOntimizeKeycloakConfiguration.java
@@ -1,7 +1,6 @@
 package com.ontimize.jee.server.security.keycloak;
 
 public interface IOntimizeKeycloakConfiguration {
-	public String[] getIgnorePaths();
 	public Boolean isPublicClient();
 	public Boolean isUseResourceRoleMappings();
 }
