76d7be2cfc1d121cd15fac42d8ce39a9d83c7909
tomfacal <88186642+tomfacal@users.noreply.github.com>
2023-10-30 08:20:52 +0100

    added not implemented methods

--- ontimize-jee-webclient-addons/src/main/java/com/ontimize/jee/webclient/export/support/exporter/DefaultSXSSFExcelExporter.java
+++ ontimize-jee-webclient-addons/src/main/java/com/ontimize/jee/webclient/export/support/exporter/DefaultSXSSFExcelExporter.java
@@ -2,6 +2,12 @@ package com.ontimize.jee.webclient.export.support.exporter;
 
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
+import com.ontimize.jee.webclient.export.exception.ExportException;
+import com.ontimize.jee.webclient.export.providers.ExportColumnProvider;
+import com.ontimize.jee.webclient.export.providers.ExportDataProvider;
+import com.ontimize.jee.webclient.export.providers.ExportStyleProvider;
+import com.ontimize.jee.webclient.export.util.ExportOptions;
+
 /**
  * Exportador que utiliza libros de tipo SXSFF, que usan streams en lugar de mantener todo el libro
  * en memoria. Es el que se usa por defecto
@@ -14,5 +20,12 @@ public class DefaultSXSSFExcelExporter extends BaseExcelExporter<SXSSFWorkbook> 
     protected SXSSFWorkbook buildWorkBook() {
         return new SXSSFWorkbook();
     }
+
+	@Override
+	public SXSSFWorkbook export(ExportColumnProvider columnProvider, ExportDataProvider dataProvider,
+			ExportStyleProvider styleProvider, ExportOptions exportOptions, boolean lanscape) throws ExportException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
